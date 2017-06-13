32a1aa3dc7281c9fa8930a177bb943445c466c0f
LaloEspino <lalospn@gmail.com>
2017-06-12 20:23:31 -0500

    Script
    Se corrige el script de la base de datos

--- script.sql
+++ script.sql
@@ -3,25 +3,25 @@ create database waddbp;
 use waddbp;
 
 create table User (
-    id int(11) not null auto_increment,
+    idU int(11) not null auto_increment,
+    type int(11),
     name varchar(20),
     password varchar(20),
-    type int(1),
-    group int(11),
-    primary key (id));
+    class int(11),
+    primary key (idU));
 
 create table Chart (
-    id int(11) not null auto_increment,
+    idC int(11) not null auto_increment,
     name varchar(20),
     x1 int(11),
     y1 int(11),
     x2 int(11),
     y2 int(11),
-    primary key (id));
+    primary key (idC));
 
-create table Commentary (
-    id int(11) not null auto_increment,
+create table Description (
+    idD int(11) not null auto_increment,
     user int(11),
-    group int(11),
-    commentary varchar(50), 
-    primary key (id));
+    groupc int(11),
+    commentary varchar(50),
+    primary key (idD));
